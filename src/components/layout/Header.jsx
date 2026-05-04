b92bf5e6bf01490c1f6a919af6b77c2ac380eebb
tomoya366050-bit <tomoya366050@gmail.com>
2026-05-04 21:23:24 +0900

    ヘッダーの変更

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,7 +4,7 @@ import ChicTypography from '../common/ChicTypography';
 const Header = () => {
   return (
     <header style={{ textAlign: 'center', marginBottom: '40px' }}>
-      <ChicTypography variant="logo">STUDY MANAGER</ChicTypography>
+      <ChicTypography variant="logo">my study manager</ChicTypography>
     </header>
   );
 };
