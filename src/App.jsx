d8940af9f488b5f645ef3ae185d37c7264ffb1f4
tomoya366050-bit <tomoya366050@gmail.com>
2026-05-04 21:41:25 +0900

    ディレクトリ名修正

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { collection, addDoc, query, where, onSnapshot, serverTimestamp, deleteDo
 import HomeView from './components/views/Home/HomeView';
 import RecordView from './components/views/Record/RecordView';
 import HistoryView from './components/views/History/HistoryView';
-import TodoView from './components/views/todos/TodoView';
+import TodoView from './components/views/Todo/TodoView';
 import Header from './components/layout/Header';
 import BottomNav from './components/layout/BottomNav';
 
